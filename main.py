0c8d74f7b879fc693af2e1bf65c0d064a9f26708
jleuschen17 <gingobanana@gmail.com>
2021-09-14 22:28:44 -0500

    Add files to github repo

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from helperFunctions import *
-if __name__ == '__main__':
-    teamScraper('Atlanta Hawks', '2010')
+if __name__ == '__main__':\
+    #Scraping league stats from 2010
+    save_df(leagueScraper('2010'), 'League2010', r"#Input computer path here#")
+    csv = r"#path to new csv"
+    df = csv_to_df(csv)
+    cleaneddf = clean_stats(df)
+    save_df(cleaneddf, 'League2010cleaned', r"#Input computer path here#")
+    standerdized_df = standerdize_stats(cleaneddf)
+    save_df(standerdized_df, 'League2010standerdized', r"#Input computer path here#")
